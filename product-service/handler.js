b6d069749f779399ecaa87996fcc56b2acd6df52
Nataliia Shyriaieva <Nataliia_Shyriaieva@epam.com>
2024-03-25 15:26:09 +0200

    feat: add function addProductById

--- product-service/handler.js
+++ product-service/handler.js
@@ -8,3 +8,21 @@ module.exports.getProductsList = async (event) => {
 		body: JSON.stringify(products),
 	};
 };
+
+module.exports.getProductById = async (event) => {
+	const products = require('./mocks/products.json');
+	const { id } = event.pathParameters;
+	const product = products.find((product) => product.id === id);
+
+	if (!product) {
+		return {
+			statusCode: 404,
+			body: JSON.stringify({ message: 'Product not found' }),
+		};
+	}
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify(product),
+	};
+};
