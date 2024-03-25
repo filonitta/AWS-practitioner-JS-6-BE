e106c81b8353b68b449a29f65b99dcdcff168160
Nataliia Shyriaieva <Nataliia_Shyriaieva@epam.com>
2024-03-25 11:54:28 +0200

    feat: add mock products

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,15 +1,10 @@
 'use strict';
 
 module.exports.getProductsList = async (event) => {
+	const products = require('./mocks/products.json');
+
 	return {
 		statusCode: 200,
-		body: JSON.stringify([
-			{
-				name: 'ProductOne',
-				price: 2.4,
-				currency: '$',
-				image: 'https://via.placeholder.com/150',
-			},
-		]),
+		body: JSON.stringify(products),
 	};
 };
