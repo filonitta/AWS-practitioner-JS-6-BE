99b11d4a54fe1e59fc2e73e8a27c367daa7f698e
Nataliia Shyriaieva <Nataliia_Shyriaieva@epam.com>
2024-04-20 13:17:18 +0300

    refactor:  delete loadData function

--- product-service/handler.js
+++ product-service/handler.js
@@ -3,5 +3,4 @@ export { getProductById } from './functions/getProductById';
 export { createProduct } from './functions/createProduct';
 export { deleteProduct } from './functions/deleteProduct';
 export { loadProducts, loadStocks } from './functions/loadProducts';
-export { loadData } from './functions/loadData';
 export { catalogBatchProcess } from './functions/catalogBatchProcess';
